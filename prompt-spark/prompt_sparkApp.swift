85efed99499f9f974f514252de5001b0de6e110b
mlhiter <3076438032@qq.com>
2025-10-24 18:55:04 +0800

    Initial Commit

---
+++ prompt-spark/prompt_sparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  prompt_sparkApp.swift
+//  prompt-spark
+//
+//  Created by 李晨洋 on 2025/10/24.
+//
+
+import SwiftUI
+
+@main
+struct prompt_sparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
